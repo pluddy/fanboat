0b690a83a99459bb9f27798e58a4358db049d4f8
Patrick Luddy <pluddy@outlook.com>
2015-09-11 10:18:29 -0500

    added fan boat ll files to workspace from course website

---
+++ devel/share/fanboat_ll/cmake/fanboat_ll-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(fanboat_ll_MESSAGE_FILES "/media/psf/Home/Documents/GitHub/fanboat/src/fanboat_ll/msg/fanboatLL.msg;/media/psf/Home/Documents/GitHub/fanboat/src/fanboat_ll/msg/fanboatMotors.msg")
+set(fanboat_ll_SERVICE_FILES "")
