fdfd5a5e4c51dfab53ec7192bf942b78ac8e0173
Patrick Luddy <pluddy@outlook.com>
2015-09-14 10:46:58 -0500

    fixed connection to motors, working state

--- src/lab1/src/xboxjoy.cpp
+++ src/lab1/src/xboxjoy.cpp
@@ -3,6 +3,7 @@
 #include <ros/ros.h>
 #include <sensor_msgs/Joy.h>
 #include <geometry_msgs/Twist.h>
+#include <fanboat_ll/fanboatMotors.h>
 // %EndTag(INCLUDE)%
 // %Tag(CLASSDEF)%
 class xboxjoy
@@ -15,7 +16,7 @@ private:
   
   ros::NodeHandle nh_;
 
-  int linear_, angular_;
+  int left_, right_;
   double l_scale_, a_scale_;
   ros::Publisher vel_pub_;
   ros::Subscriber joy_sub_;
@@ -24,17 +25,17 @@ private:
 // %EndTag(CLASSDEF)%
 // %Tag(PARAMS)%
 xboxjoy::xboxjoy():
-  linear_(1),
-  angular_(2)
+  left_(2),
+  right_(5)
 {
 
-  nh_.param("axis_linear", linear_, linear_);
-  nh_.param("axis_angular", angular_, angular_);
-  nh_.param("scale_angular", a_scale_, a_scale_);
-  nh_.param("scale_linear", l_scale_, l_scale_);
+//  nh_.param("axis_linear", linear_, linear_);
+//  nh_.param("axis_angular", angular_, angular_);
+//  nh_.param("scale_angular", a_scale_, a_scale_);
+//  nh_.param("scale_linear", l_scale_, l_scale_);
 // %EndTag(PARAMS)%
 // %Tag(PUB)%
-  vel_pub_ = nh_.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 1);
+  vel_pub_ = nh_.advertise<fanboat_ll::fanboatMotors>("motors", 1);
 // %EndTag(PUB)%
 // %Tag(SUB)%
   joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &xboxjoy::joyCallback, this);
@@ -43,10 +44,16 @@ xboxjoy::xboxjoy():
 // %Tag(CALLBACK)%
 void xboxjoy::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 {
-  geometry_msgs::Twist vel;
-  vel.angular.z = a_scale_*joy->axes[angular_];
-  vel.linear.x = l_scale_*joy->axes[linear_];
-  vel_pub_.publish(vel);
+  fanboat_ll::fanboatMotors boat;
+  double r = joy->axes[right_];
+  double l = joy->axes[left_];
+  ROS_INFO("%f %f",l,r);
+  r = (1 - r) / 2;
+  l = (1 - l) / 2;
+  boat.left = l;
+  boat.right = r;
+  ROS_INFO("%f %f",l,r);
+  vel_pub_.publish(boat);
 }
 // %EndTag(CALLBACK)%
 // %Tag(MAIN)%
