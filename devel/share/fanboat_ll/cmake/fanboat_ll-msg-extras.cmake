3064090168024fa9552a0b8a6d8773ab8c4370a0
toan <trant1938@gmail.com>
2015-09-23 11:50:36 -0500

    launch file added

--- devel/share/fanboat_ll/cmake/fanboat_ll-msg-extras.cmake
+++ devel/share/fanboat_ll/cmake/fanboat_ll-msg-extras.cmake
@@ -1,2 +1,2 @@
-set(fanboat_ll_MESSAGE_FILES "/home/viki/catkin_ws/src/fanboat/src/fanboat_ll/msg/fanboatLL.msg;/home/viki/catkin_ws/src/fanboat/src/fanboat_ll/msg/fanboatMotors.msg")
+set(fanboat_ll_MESSAGE_FILES "/home/toan/fanboat/src/fanboat_ll/msg/fanboatLL.msg;/home/toan/fanboat/src/fanboat_ll/msg/fanboatMotors.msg")
 set(fanboat_ll_SERVICE_FILES "")
