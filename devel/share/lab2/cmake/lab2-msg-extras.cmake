b14d6bf2d3725b5417e4cafe7ff9c744373284d9
Jakob Snyder <jakobjsnyder@gmail.com>
2015-10-14 17:53:19 -0500

    doing more work on reactive control

--- devel/share/lab2/cmake/lab2-msg-extras.cmake
+++ devel/share/lab2/cmake/lab2-msg-extras.cmake
@@ -1,2 +1,2 @@
-set(lab2_MESSAGE_FILES "/home/cody/catkin_ws/src/fanboat/src/lab2/msg/angle_msg.msg")
+set(lab2_MESSAGE_FILES "/home/viki/catkin_ws/src/fanboat/src/lab2/msg/angle_msg.msg")
 set(lab2_SERVICE_FILES "")
--- src/lab2/src/reactiveControl.cpp
+++ src/lab2/src/reactiveControl.cpp
@@ -12,8 +12,7 @@ public:
 	//Private functions/variables
 private:
 	//void angle_callback(const lab2::angle_msg::ConstPtr& ang);
-	void fanboatLL_callback(const fanboat_ll::fanboatLL::ConstPtr& ang);
-
+	void sensorFilter_callback(const fanboat_ll::fanboatLL::ConstPtr& ang);
 	ros::NodeHandle nh_;
 
 	ros::Publisher angles_pub_;
@@ -29,7 +28,7 @@ double leftScale;
 reactiveControl::reactiveControl()
 {
 	//Subscribe to arbitated angle messages
-	sensors_sub_ = nh_.subscribe<fanboat_ll::fanboatLL>("sensors", 1, &reactiveControl::fanboatLL_callback, this);
+	sensors_sub_ = nh_.subscribe<fanboat_ll::fanboatLL>("sensors", 1, &reactiveControl::sensorFilter_callback, this);
 
 
 	//Publish to angles topic
@@ -39,10 +38,12 @@ reactiveControl::reactiveControl()
 
 //Variables
 fanboat_ll::fanboatLL fll;
-uint16_t initLeftS = -100;
-uint16_t initRightS = -100;
-double angle, thrust;
+double initLeftS = -100;
+double initRightS = -100;
 double init = -1000;
+double leftS, rightS, rightMotor, leftMotor, angle, thrust, diffL, diffR;
+bool isMapping = false;
+bool isForward = true;
 
 //pulled from the arduino library.
 double map(double x, double in_min, double in_max, double out_min, double out_max)
@@ -50,41 +51,82 @@ double map(double x, double in_min, double in_max, double out_min, double out_ma
 	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-void reactive(int leftSensor, int rightSensor)
+void reactive()
 {
+	diffL = initLeftS - leftS;
+	diffR = initRightS - rightS;
+
+	if(diffL > -50 && diffL < 50 && diffR > -50 && diffR < 50){
+		isForward = true;
+	}
+	else{
+		isForward = false;
+
+
+	}
+
 	//right = .0;
 	//left = .0;
+	ROS_INFO("reactiveControlFunction");
 
-	ROS_INFO("leftS = %d rightS = %d",leftSensor,rightSensor);
+	ROS_INFO("leftS = %lf rightS = %lf diffL = %lf diffR = %lf",leftS,rightS, diffL, diffR);
 }
 
 
-
+//we want the boat to stay within 350-425 range finders for sensors
 //Get feedback from fanboat
-void reactiveControl::fanboatLL_callback(const fanboat_ll::fanboatLL::ConstPtr& f_ll)
+void reactiveControl::sensorFilter_callback(const fanboat_ll::fanboatLL::ConstPtr& f_ll)
 {
+	fll = *f_ll;
 	lab2::angle_msg msg;
-	uint16_t leftS;
-	uint16_t rightS;
+	
+	ROS_INFO("reactiveControl");
 	leftS = fll.a0;
-	rightS = fll.a0;
+	rightS = fll.a1;
 	if (initLeftS == -100){
 		initLeftS = leftS;
 	}
 	if (initRightS == -100){
 		initRightS = rightS;
 	}
-	reactive(leftS,rightS);
-	msg.angle = 0.0;
-	msg.thrust = .3;
+	reactive();
 	//motors_pub_.publish(boat);
 }
+
+double getAngle(){
+	return 0.0;
+}
+
 
 int main(int argc, char** argv)
 {
 	
 	ros::init(argc, argv, "reactiveControl");
 	reactiveControl reactiveControl;
+	lab2::angle_msg boat;
+	ros::Rate loop_rate(12);//sets the f to 12Hz
+	ros::NodeHandle nh;
+
+	
+	while(ros::ok()) {
+		if(isMapping){
+			ROS_INFO(" ");
+
+		}
+		else if(isForward){
+			boat.angle = 0;
+			boat.thrust = .35;
+		}
+		else if(!isForward){
+			boat.angle = getAngle();
+			boat.thrust = 0;
+		}
+
+		//angles_pub_.publish(boat);
+		ros::spinOnce();
+		loop_rate.sleep();
+	}
+
 
 	ros::spin();
 }
