bbf499f9278edd26c9960ab406ea12578071cf27
Jakob Snyder <jakobjsnyder@gmail.com>
2015-11-14 03:56:58 +1030

    Merged all the work we did

--- devel/share/lab3/cmake/lab3-msg-paths.cmake
+++ devel/share/lab3/cmake/lab3-msg-paths.cmake
@@ -1,8 +1,4 @@
 # generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
 
-<<<<<<< HEAD
 set(lab3_MSG_INCLUDE_DIRS "/home/odroid/catkin_ws/src/fanboat/src/lab3/msg")
-=======
-set(lab3_MSG_INCLUDE_DIRS "/home/viki/catkin_ws/src/fanboat/src/lab3/msg")
->>>>>>> 8f00e7774a103ee680e8144a155c28aad7a2b6cf
 set(lab3_MSG_DEPENDENCIES std_msgs)
--- src/lab3/src/visualArbitrator.cpp
+++ src/lab3/src/visualArbitrator.cpp
@@ -45,11 +45,20 @@ VisualArbitrator::VisualArbitrator()
 
 	bool lookForBall = true;
 	void VisualArbitrator::imageCb(const sensor_msgs::ImageConstPtr& msg){
+		ROS_INFO("Test %d", msg->height);
+		sensor_msgs::Image image;
+		image.header = msg->header;
+		image.height = msg->height;
+		image.width = msg->width;
+		image.encoding = msg->encoding;
+		image.is_bigendian = msg->is_bigendian;
+		image.step = msg->step;
+		image.data = msg->data;
 		if(lookForBall == true){
-			ball_img_pub_.publish(*msg);
+			ball_img_pub_.publish(image);
 		}
 		else{
-			land_img_pub_.publish(*msg);
+			land_img_pub_.publish(image);
 		}
 	}
 
