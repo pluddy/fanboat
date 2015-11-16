8d85047b868d952ce0b4206a67e616fcb02137de
Jakob Snyder <jakobjsnyder@gmail.com>
2015-11-17 05:28:42 +1030

    Fixed visualArbitrator from deathly chaos

--- src/lab3/src/visualArbitrator.cpp
+++ src/lab3/src/visualArbitrator.cpp
@@ -17,51 +17,36 @@ public:
 private:
 
 	void imageCb(const sensor_msgs::ImageConstPtr& msg);
-//	void has_ball_callback(const std_msgs::Bool::ConstPtr& msg);
+	//	void has_ball_callback(const std_msgs::Bool::ConstPtr& msg);
 
 
 
 	ros::NodeHandle nh_;
 	image_transport::ImageTransport it;
 	image_transport::Subscriber image_sub_;
-//	ros::Subscriber has_ball_sub_;
+	//	ros::Subscriber has_ball_sub_;
 	image_transport::Publisher ball_img_pub_;
-  image_transport::Publisher land_img_pub_;
-
+	image_transport::Publisher land_img_pub_;
 };
 
 //Set values to read from xbox controller
-VisualArbitrator::VisualArbitrator()
-:it(nh_)
-	{
-		//Subscribe to joy_constant for button controls to arbitrate
-  	image_sub_ = it.subscribe("/usb_cam/image_raw", 1, &VisualArbitrator::imageCb, this);
-		//has_ball_sub_ = nh_.subscribe("hasBall", 1, &visualArbitrator::has_ball_callback, this);
-		image_transport::Publisher land_img_pub_ = it.advertise("/usb_cam/image_raw_land", 3);
- 		image_transport::Publisher ball_img_pub_ = it.advertise("/usb_cam/image_raw_ball", 3);
-	//	ball_img_pub_ = it.advertise<sensor_msgs::Image>("/usb_cam/image_raw_ball", 10);
-		//land_img_pub_ = it.advertise<sensor_msgs::Image>("/usb_cam/image_raw_landmark", 1);
-	}
+VisualArbitrator::VisualArbitrator() :it(nh_)
+{
+	image_sub_ = it.subscribe("/usb_cam/image_raw", 1, &VisualArbitrator::imageCb, this);
 
-	bool lookForBall = true;
-	void VisualArbitrator::imageCb(const sensor_msgs::ImageConstPtr& msg){
-		ROS_INFO("Test %d", msg->height);
-		sensor_msgs::Image image;
-		image.header = msg->header;
-		image.height = msg->height;
-		image.width = msg->width;
-		image.encoding = msg->encoding;
-		image.is_bigendian = msg->is_bigendian;
-		image.step = msg->step;
-		image.data = msg->data;
-		if(lookForBall == true){
+	land_img_pub_ = it.advertise("usb_cam/image_raw_land", 3);
+	ball_img_pub_ = it.advertise("usb_cam/image_raw_ball", 3);
+}
 
-			ball_img_pub_.publish(msg);
-		}
-		else{
-			land_img_pub_.publish(msg);
-		}
+bool lookForBall = true;
+void VisualArbitrator::imageCb(const sensor_msgs::ImageConstPtr& image){
+	if(lookForBall){
+		ball_img_pub_.publish(*image);
 	}
+	else{
+		land_img_pub_.publish(*image);
+	}
+}
 
 int main(int argc, char** argv)
 {
